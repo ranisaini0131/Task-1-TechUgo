c70f39e24ff6cd5fbe2e4f013d90b163df64fc3c
Rani Saini <sainirani1797@gmail.com>
2024-06-07 17:51:00 +0530

    nodemailer bug fixed

--- src/utility/nodemailer.utility.js
+++ src/utility/nodemailer.utility.js
@@ -1,16 +1,30 @@
 //configuation for sending email from gmail
 import nodemailer from "nodemailer"
+import dotenv from "dotenv"
+dotenv.config({
+    path: "./.env"
+})
 
 let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    // host: "smtp.gmail.com",
+    // port: 587,
+    // secure: false, // true for 465, false for other ports
+    service: "gmail",
     auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD
     }
 })
 
+console.log(process.env.EMAIL, "15")
+
+//Verify Transporter
+transporter.verify((err, success) => {
+    if (err) { console.error(err); }
+    else {
+        console.log('Your config is correct');
+    }
+})
 
 export const sendMail = async function ({ to, subject, html }) {
     let info = await transporter.sendMail({
